45bba413b4e3a89666a5f2a1b4970e1df39d998c
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-10-01 14:53:34 +0200

    MongoDB: Generate JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 import { User, UserSchema } from './entities/user.entity';
 import { AuthController } from './auth.controller';
@@ -17,6 +18,11 @@ import { AuthController } from './auth.controller';
         schema: UserSchema,
       },
     ]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SEED,
+      signOptions: { expiresIn: '6h' },
+    }),
   ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,17 +8,21 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import * as bcryptjs from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { LoginDto } from './dto/login.dto';
+import { JwtPayload } from './interfaces/jwt-payload';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name)
     private userModel: Model<User>,
+
+    private jwtService: JwtService,
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
@@ -60,7 +64,7 @@ export class AuthService {
 
     return {
       user: userData,
-      token: 'ABC-123',
+      token: this.getJWToken({ id: user.id }),
     };
   }
 
@@ -78,5 +82,11 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  getJWToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+
+    return token;
   }
 }
